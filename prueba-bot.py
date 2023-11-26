c9f2de68ca5eb9080537153d3e827002dad5341e
Beltonidas <tomasschuenemann@outlook.com>
2023-11-26 17:33:50 -0300

    bandas-bollinger

--- prueba-bot.py
+++ prueba-bot.py
@@ -1,26 +1,120 @@
+
+import datetime  # For datetime objects
+import os.path  # To manage paths
+import sys  # To find out the script name (in argv[0])
+
+# Import the backtrader platform
 import backtrader as bt
-import datetime as dt
-from strategy import TestStrategy
+import inline as inline
+import matplotlib
 
-cerebro = bt.Cerebro()
-cerebro.broker.setcash(100000)
 
-# Create a Data Feed
-data = bt.feeds.YahooFinanceCSVData(
-        dataname='data-base-oracle.csv',
+# Create a Stratey
+class TestStrategy(bt.Strategy):
+
+    def log(self, txt, dt=None):
+        ''' Logging function fot this strategy'''
+        dt = dt or self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
+
+    def __init__(self):
+        # Keep a reference to the "close" line in the data[0] dataseries
+        self.dataclose = self.datas[0].close
+
+        # To keep track of pending orders
+        self.order = None
+
+        self.sma = bt.ind.SMA(period=200)
+        self.boll = bt.ind.BollingerBands(period=20, devfactor=2.5)
+        self.rsi = bt.ind.RSI(period=2, safediv=True)
+
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            # Buy/Sell order submitted/accepted to/by broker - Nothing to do
+            return
+
+        # Check if an order has been completed
+        # Attention: broker could reject order if not enough cash
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.log('BUY EXECUTED, %.2f' % order.executed.price)
+            elif order.issell():
+                self.log('SELL EXECUTED, %.2f' % order.executed.price)
+
+            self.bar_executed = len(self)
+
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            self.log('Order Canceled/Margin/Rejected')
+
+        # Write down: no pending order
+        self.order = None
+
+    def next(self):
+        # Simply log the closing price of the series from the reference
+        self.log('Close, %.2f' % self.dataclose[0])
+
+        # Check if an order is pending ... if yes, we cannot send a 2nd one
+        if self.order:
+            return
+
+        # Check if we are in the market
+        if not self.position:
+
+            if ((self.data.close > self.sma) and (self.data.close < self.boll.lines.bot)):
+                    # BUY, BUY, BUY!!! (with default parameters)
+                    self.log('BUY CREATE, %.2f' % self.dataclose[0])
+
+                    # Keep track of the created order to avoid a 2nd order
+                    self.order = self.buy(exectype=bt.Order.Limit, price=0.80 * self.dataclose[0])
+
+        else:
+
+            # Already in the market ... we might sell
+            if ((self.rsi > 50) or (len(self) >= (self.bar_executed + 10))):
+                # SELL, SELL, SELL!!! (with all possible default parameters)
+                self.log('SELL CREATE, %.2f' % self.dataclose[0])
+
+                # Keep track of the created order to avoid a 2nd order
+                self.order = self.sell()
+
+
+if __name__ == '__main__':
+    # Create a cerebro entity
+    cerebro = bt.Cerebro()
+
+    # Add a strategy
+    cerebro.addstrategy(TestStrategy)
+
+    # Datas are in a subfolder of the samples. Need to find where the script is
+    # because it could have been called from anywhere
+    modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
+    datapath = os.path.join(modpath, 'orcl-1995-2014.txt')
+
+    # Create a Data Feed
+    data = bt.feeds.YahooFinanceCSVData(
+        dataname=datapath,
         # Do not pass values before this date
-        fromdate=dt.datetime(2000, 1, 1),
+        fromdate=datetime.datetime(2000, 1, 1),
+        # Do not pass values before this date
+        todate=datetime.datetime(2000, 12, 31),
         # Do not pass values after this date
-        todate=dt.datetime(2000, 12, 31),
         reverse=False)
 
-cerebro.adddata(data)
-cerebro.addstrategy(TestStrategy)
+    # Add the Data Feed to Cerebro
+    cerebro.adddata(data)
 
-print('Mi portafolio inicial:  %.2f' % cerebro.broker.getvalue())
-cerebro.run()
+    # Set our desired cash start
+    cerebro.broker.setcash(1000)
+    cerebro.broker.setcommission(commission=0.005)
 
+    # Print out the starting conditions
+    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
 
-print('Mi portafolio inicial:  %.2f' % cerebro.broker.getvalue())
+    # Run over everything
+    cerebro.run()
 
-cerebro.plot()
+    # Print out the final result
+    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    cerebro.plot()
+
